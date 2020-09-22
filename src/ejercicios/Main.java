45013bd541fe4f14493e563ce86f868c43fb34de
Andrés Segura <63387323+Andr7st@users.noreply.github.com>
2020-09-21 22:11:16 -0500

    Add files via upload
    subido con drag and drop, mas me gustaba como era antes.

---
+++ src/ejercicios/Main.java
@@ -0,0 +1,15 @@
+package ejercicios;
+/**
+ * @author Andrés Segura
+ * - Ejercicios de programación java Java.
+ * - El método Main es el único ejecutable.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        AdivinaNumeroV1 ejercicio1 = new AdivinaNumeroV1();
+        ejercicio1.Run();
+
+    }
+}
